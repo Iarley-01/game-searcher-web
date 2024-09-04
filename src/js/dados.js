a175e401b51a00852d14e73c2a75b4fdbcb4cd53
Iarley-01 <yarleyalves2016@gmail.com>
2024-09-04 16:19:20 -0300

    criando base de dados em javaScript

---
+++ src/js/dados.js
@@ -0,0 +1,50 @@
+const jogos = [
+    {
+      linkCapa: "https://exemplo.com/imagem-god-of-war2.jpg",
+      titulo: "God of War II",
+      descricao: "Uma sequência épica, com batalhas ainda mais intensas e um enredo envolvente.",
+      link: "https://www.playstation.com.com/en-us/games/god-of-war-ii/",
+    },
+    {
+      linkCapa: "https://exemplo.com/imagem-grand-theft-auto-san-andreas.jpg",
+      titulo: "Grand Theft Auto: San Andreas",
+      descricao: "Um mundo aberto imenso e detalhado, com uma história épica.",
+      link: "https://www.rockstargames.com/gta3",
+    },
+    {
+      linkCapa: "https://exemplo.com/imagem-metal-gear-solid-3.jpg",
+      titulo: "Metal Gear Solid 3: Snake Eater",
+      descricao: "Um dos melhores jogos da série, com uma história emocionante e jogabilidade inovadora.",
+      link: "https://www.konami.com/games/metalgear/",
+    },
+    {
+      linkCapa: "https://exemplo.com/imagem-monster-hunter-freedom.jpg",
+      titulo: "Monster Hunter Freedom",
+      descricao: "O primeiro Monster Hunter a chegar no ocidente, iniciando uma franquia de sucesso.",
+      link: "https://www.capcom.co.jp/monsterhunter/",
+    },
+    {
+      linkCapa: "https://exemplo.com/imagem-persona-3-portable.jpg",
+      titulo: "Persona 3 Portable",
+      descricao: "Um RPG tático com elementos de vida escolar, com uma história profunda.",
+      link: "https://www.atlus.com/persona/",
+    },
+    {
+      linkCapa: "https://exemplo.com/imagem-persona-3-portable.jpg",
+      titulo: "Burnout Legends",
+      descricao: "Burnout Legends é um jogo de corrida frenético que traz a intensidade da série Burnout para o portátil PSP.",
+      link: "https://en.m.wikipedia.org/wiki/Burnout_Legends",
+    },
+    {
+      linkCapa: "https://exemplo.com/imagem-persona-3-portable.jpg",
+      titulo: "Midnight Club 3: Dub Edition",
+      descricao: "Midnight Club 3: Dub Edition traz a emoção das corridas de rua para o PSP, com gráficos impressionantes e uma trilha sonora vibrante.",
+      link: "https://pt.m.wikipedia.org/wiki/Midnight_Club_3:_DUB_Edition",
+    },
+    {
+      linkCapa: "https://exemplo.com/imagem-persona-3-portable.jpg",
+      titulo: "Gran Turismo",
+      descricao: "Gran Turismo PSP oferece uma experiência de simulação de corrida realista, com uma vasta gama de carros e pistas para explorar.",
+      link: "https://en.m.wikipedia.org/wiki/Gran_Turismo_(2009_video_game)",
+    }
+];
