77a250c2d6165fdfcf5d7404357013f759905395
Iarley-01 <yarleyalves2016@gmail.com>
2024-09-05 17:35:24 -0300

    adicionando informações dos cards pelo javaScript

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
+let containerResultados = document.getElementById("resultados-pesquisa");
 
+let resultados = "";
+
+for (let jogo of jogos) {
+  resultados += `
+    <div class="item-resultado">
+      <h2>${jogo.titulo}</h2>
+      <p class="descricao-meta">${jogo.descricao}</p>
+      <a href="${jogo.linkInfo}" target="_blank">Mais Informações</a>
+    </div>
+  `
+  containerResultados.innerHTML = resultados;
+}
 console.log(dados);
