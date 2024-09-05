a336f60d12d2267783c548b94008c5d66e8b1747
Iarley-01 <yarleyalves2016@gmail.com>
2024-09-05 19:43:21 -0300

    criando funcionalidade de pesquisar

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,18 @@ let containerResultados = document.getElementById("resultados-pesquisa");
 
 let resultados = "";
 
-for (let jogo of jogos) {
-  resultados += `
-    <div class="item-resultado">
-      <h2>${jogo.titulo}</h2>
-      <p class="descricao-meta">${jogo.descricao}</p>
-      <a href="${jogo.linkInfo}" target="_blank">Mais Informações</a>
-    </div>
-  `
-  containerResultados.innerHTML = resultados;
+function pesquisar() {
+  for (let jogo of jogos) {
+    resultados += `
+      <div class="item-resultado">
+        <h2>${jogo.titulo}</h2>
+        <p class="descricao-meta">${jogo.descricao}</p>
+        <a href="${jogo.linkInfo}" target="_blank">Mais Informações</a>
+      </div>
+    `;
+    containerResultados.innerHTML = resultados;
+  }
 }
+
+
 console.log(dados);
