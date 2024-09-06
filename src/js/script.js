43c60632f4ca0b5331a8e75e5eea0508d86891c9
Iarley-01 <yarleyalves2016@gmail.com>
2024-09-06 18:16:29 -0300

    melhorando funcionalidade de busca

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,40 @@
-let containerResultados = document.getElementById("resultados-pesquisa");
 
-let resultados = "";
 
 function pesquisar() {
+  
+  let containerResultados = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa");
+  
+  let termoDePesquisa = campoPesquisa.value;
+  
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+  
+  if (termoDePesquisa == "" || termoDePesquisa.includes(" ")) {
+    containerResultados.innerHTML = `<h2 class="mensagem-aviso">Nada foi encontrado, digite um termo válido!</h2>`;
+    return;
+  }
+  
+
   for (let jogo of jogos) {
-    resultados += `
+    titulo = jogo.titulo.toLowerCase();
+    descricao = jogo.descricao.toLowerCase();
+    
+    if (titulo.includes(termoDePesquisa.toLowerCase()) || descricao.includes(termoDePesquisa.toLowerCase())) {
+      resultados += `
       <div class="item-resultado">
         <h2>${jogo.titulo}</h2>
         <p class="descricao-meta">${jogo.descricao}</p>
         <a href="${jogo.linkInfo}" target="_blank">Mais Informações</a>
       </div>
     `;
+    if (!resultados) {
+    containerResultados.innerHTML = `<h2>Nenhum resultado foi encontrado. Digite o nome do jogo ou genero</h2>`;
+  }
     containerResultados.innerHTML = resultados;
+    }
+    
   }
 }
-
-
-console.log(dados);
